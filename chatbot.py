270412901d7ad34c2f91ea87bd452d3e326cfb68
Niharika <niharikamahajan47@gmail.com>
2026-09-05 15:17:25 +0530

    Added context history to chatbot

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,17 @@ load_dotenv()
 llm=HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V3-0324"
 )
+chat_history=[]
 
 model=ChatHuggingFace(llm=llm)
 
 while True:
     user_inp=input("you:")
-    res=model.invoke(user_inp)
-
+    chat_history.append(user_inp)
+    res=model.invoke(chat_history)
+    chat_history.append(res.content)
     print("Ai",res.content)
 
+
+
+print(chat_history)
