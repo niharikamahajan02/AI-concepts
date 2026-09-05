f4180f011ab64b16ae2cbef08a22be1abc1c3ab9
Niharika <niharikamahajan47@gmail.com>
2026-09-05 13:07:01 +0530

    Dynamic prompt added

--- python_ui.py
+++ python_ui.py
@@ -1,6 +1,7 @@
 from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.prompts import PromptTemplate
 
 load_dotenv()
 
@@ -10,14 +11,27 @@ llm = HuggingFaceEndpoint(
     repo_id="deepseek-ai/DeepSeek-V3-0324",
     task="text-generation"
 )
-
+i1=st.selectbox("slect option",['option1','optionh2'])
+i2=st.selectbox("slect option",['option1','option2'])
 model = ChatHuggingFace(llm=llm)
 
+#make prompt template
+template=PromptTemplate(
+    template="""use here {i1}then here {i2}""",
+
+    input_variables=['i1','i2']
+
+)
+#fill prompt template
+prompt=template.invoke({
+    'i1':i1,
+    'i2':i2
+})
 user_in = st.text_input("Enter prompt")
 
 if st.button("Summarize"):
     if user_in:
-        res = model.invoke(user_in)
+        res = model.invoke(prompt)
         st.write(res.content)
     else:
         st.warning("Please enter a prompt")
