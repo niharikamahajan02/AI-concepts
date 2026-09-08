a3297237c2b5123b3dc70bdda4abc69343611edc
Niharika <niharikamahajan47@gmail.com>
2026-09-08 22:32:33 +0530

    llm with pydantic

--- structured_output/pydantic_demo.py
+++ structured_output/pydantic_demo.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 #pip install pydantic
 
-from pydantic import BaseModel 
+from pydantic import BaseModel ,Field 
 
 class Student(BaseModel):
     name:str
@@ -13,4 +13,39 @@ class Student(BaseModel):
 new_str={'name':'nitish'}
 
 student=Student(**new_str)
+s_dict=dict(student)
+
+print(s_dict['name'])
 print(student)
+
+
+
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from dotenv import load_dotenv
+from typing import Optional,Literal
+
+load_dotenv()
+
+llm=HuggingFaceEndpoint(
+ repo_id="deepseek-ai/DeepSeek-V3-0324"
+)
+
+model=ChatHuggingFace(llm=llm)
+
+class review(BaseModel):
+
+    key_theme:list[str]=Field(description="write key themes discu in review")
+    summary:str=Field(description="a brief summary of review") #string vbhejan with kya description
+    sentiment:str
+    pros:Optional[list[str]]=Field(default=None, description="pros of prdoct in review")
+
+
+structed_model=model.with_structured_output(review)
+
+#During invikation a system promp also goes with the human msg as u r a ai aaistant that extracts insights from text ,
+#  give poprdc review ssummary:brief oiobvverview, sentiment : tone of review . return res in jSON
+res=structed_model.invoke("this is a parcel which is better than other parcels. Also its designed very elligently ,,Godd for dwaily uses")
+
+print(res.summary)
+print(res.sentiment)
+
